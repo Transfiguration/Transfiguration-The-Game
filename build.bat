6de626e7fd1c103ecd55765a6efea617e0decc15
Transfiguration <transfiguration.developer@gmail.com>
2021-05-06 10:56:31 +0200

    Updated the build.bat file to install tweego if necessary

--- build.bat
+++ build.bat
@@ -1,4 +1,35 @@
-@ECHO off
-ECHO Building Transfiguration
+@echo off
+:check_if_tweego_installed
+where tweego >nul
+if %ERRORLEVEL% equ 0 (
+  goto build
+)
+
+:install_tweego
+echo Tweego was not installed, installing tweego v1.2.2
+powershell -Command "Invoke-WebRequest https://github.com/ChapelR/tweego-installer/releases/download/v1.2.2/tweego-installer.exe -OutFile tweego-installer.exe"
+echo Downloaded tweego installer
+start /wait tweego-installer.exe
+del tweego-installer.exe
+:manually_check_path_to_avoid_restart
+where tweego >nul
+if %ERRORLEVEL% equ 0 (
+  goto build
+)
+if exist "C:\Program Files\Tweego\tweego.exe" (
+  set PATH=%PATH%;C:\Program Files\Tweego\tweego.exe
+)
+where tweego >nul
+if %ERRORLEVEL% equ 0 (
+  goto build
+)
+echo Restart CMD
+pause
+exit
+
+:build
+echo Building Transfiguration with tweego v1.2.2
 tweego -o transfiguration.html Transfiguration-The-Game %*
-ECHO Done
+if %ERRORLEVEL% equ 0 (
+  echo Done
+)
